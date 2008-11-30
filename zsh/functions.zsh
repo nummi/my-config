abb20f50af728843a23740426d698a3df09f74f4
Jerry Nummi <jerry@theedgecase.com>
2008-11-29 20:48:56 -0500

    Made zsh functions consistent

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,14 +1,14 @@
-function cd { builtin cd $1 && ls }
+function cd() { builtin cd $1 && ls }
 
-function gci { git ci -m "$*" }
+function gci() { git ci -m "$*" }
 
 function pman() { man -t "${1}" | open -f -a /Applications/Preview.app }
 
-function photoshop { open -a Adobe\ Photoshop\ CS3 $* }
+function photoshop() { open -a Adobe\ Photoshop\ CS3 $* }
 
-function preview { open -a Preview $* }
+function preview() { open -a Preview $* }
 
-function pushed {
+function pushed() {
   if [ $@ ]; then
     git cherry -v origin/$@
   else
@@ -20,7 +20,7 @@ function reload() { touch tmp/restart.txt }
 
 # Force 'sudo zsh' to start root as a loging shell to 
 # avoid problems with environment clashes:
-function sudo {
+function sudo() {
   if [[ $1 = "zsh" ]]; then
     command sudo /opt/local/bin/zsh -l
   else
