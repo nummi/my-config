37832eb0fe620189822c6ddf863e083ac7ca3fde
Jerry Nummi <jerry@theedgecase.com>
2009-04-03 12:27:05 -0400

    Added edgecase site and blog to zsh aliases.

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -5,3 +5,5 @@ bindkey "^E" end-of-line
 bindkey "^B" backward-word
 bindkey "^F" forward-word
 bindkey "^J" self-insert
+#bindkey '^[[D' backward-word # option left
+#bindkey '^[[C' forward-word # option right
