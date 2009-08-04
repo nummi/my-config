482bec4b4117a4ae75c9c484c9fa1b6acef0642f
Jerry Nummi <jerry@theedgecase.com>
2009-08-04 09:36:56 -0400

    Cleanup up zsh aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,8 +8,6 @@ alias dbfl='rake db:fixtures:load'
 alias dbmi='rake db:migrate'
 alias diff='git diff'
 alias diffmate='git diff | mate'
-alias g='git'
-alias gd='git diff | mate'
 alias grep='grep --color --line-number '
 alias la='ls -laFh'
 alias monitor_http='sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E "Host\: .*|GET \/.*"'
@@ -23,52 +21,28 @@ alias pull='git pull'
 alias push='git push'
 alias pushed\?='pushed'
 alias rc='rake cruise'
-alias restart='reload'
-alias ri='ri -Tf ansi'
 alias rmate='mate app config doc db lib public script spec test stories liquid'
 alias rrr="rake db:migrate && rake db:rollback && rake db:migrate"
 alias sc='./script/console'
-alias sfl='rake spec:db:fixtures:load'
 alias sp='./script/spec -cfs'
 alias ss='./script/server'
 alias st='clear; git status'
 alias sudo='nocorrect sudo'
 
 
-# SHORTCUTS
-
+# shortcuts
 alias projects='cd ~/Projects'
 alias webserver='cd /Library/WebServer/Documents'
 
 # personal
 alias myconfig='cd ~/Projects/config'
-alias expo='cd ~/Projects/expo'
 alias paperactive='cd ~/Projects/paperactive'
-alias sweetlabels='cd ~/Projects/sweet-labels'
-
-# sanky
-alias sanky='cd ~/Projects/sankyinc'
-
-# igv
-alias igv='cd ~/Projects/igv'
-
-# q1
-alias q1='cd ~/Projects/q1'
-alias gca_stats='cd ~/Projects/q1/gca_stats'
 
 # edgecase
-alias bikeshop='cd ~/Projects/edgecase/clients/bikeshopft'
-alias ccli='cd ~/Projects/edgecase/clients/ccli'
-alias clients='cd ~/Projects/edgecase/clients'
 alias diplomas='cd ~/Projects/edgecase/clients/diplomas';
-alias edgecase_blog='cd ~/Projects/edgecase/edgecase_blog';
-alias edgecase_site='cd ~/Projects/edgecase/edgecase.com';
 alias fxcm='cd ~/Projects/edgecase/clients/fxcm-uk'
-alias joemetric='cd ~/Projects/joemetric'
 alias prosper='cd ~/Projects/edgecase/prosper'
 alias scripsafe='cd ~/Projects/edgecase/clients/escriptsafe/scripsafe'
 alias streamdeploy='cd ~/Projects/edgecase/clients/streamdeploy'
-alias theedgecase_com='cd ~/Projects/edgecase/theedgecase.com'
 alias toobla='cd ~/Projects/edgecase/clients/toobla'
 alias transcripts='cd ~/Projects/edgecase/clients/transcripts'
-alias wildresearch='cd ~/Projects/edgecase/clients/wildresearch'
