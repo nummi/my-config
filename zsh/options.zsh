a2d22148974576a4f1348c8dabea840dc560a185
Jerry Nummi <jerry@edgecase.com>
2010-05-15 17:42:15 -0400

    ZSH options cleanup.

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,11 +1,10 @@
-setopt always_to_end            # COMPLETION   - When auto complete from middle, complete and move cursor to end of word
-setopt append_history           # HISTORY      - Append to history file, not overwrite it
-setopt auto_cd
-setopt AUTO_LIST                # COMPLETION   - Automatically list choices on an ambiguous completion
-setopt complete_in_word         # COMPLETION   - Always auto complete, not just at the end
-setopt correct                  # INPUT/OUTPUT - Try to correct the spelling of commands
-setopt HIST_IGNORE_DUPS         # HISTORY      - Don't enter command in history if it is duplicate of previous command
-setopt HIST_FIND_NO_DUPS        # HISTORY      - Don't display duplicates when searching commands
-setopt inc_append_history       # HISTORY      - Add command to history file as soon as entered, not at shell exit 
-setopt nobeep
-setopt nohup                    #              - Don't kill background jobs upon logging out
+setopt always_to_end            # When complete from middle, move cursor
+setopt append_history           # Append to history file, not overwrite it
+setopt auto_cd                  # If a command is not in the hash table, and there exists an executable directory by that name, perform the cd command to that directory.
+setopt complete_in_word         # Not just at the end
+setopt extendedglob             # Extra filename generation features
+setopt hist_ignore_dups         # Don't enter command in history if it is duplicate of previous command
+setopt hist_find_no_dups        # Don't display duplicates when searching commands
+setopt inc_append_history       # Append history item as soon as command is executed, rather than waiting until the shell is killed
+setopt nohup										# In general, we don't kill background jobs upon logging out
+setopt prompt_subst             # parameter expansion, command substitution and arithmetic expansion is performed in prompts
