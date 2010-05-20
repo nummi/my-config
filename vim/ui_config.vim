1b13c3609debec4b93c1c8c6051478ff5f23b90c
Jerry Nummi <jerry@edgecase.com>
2010-05-20 14:45:31 -0400

    Synced zsh aliases with EdgeCase pairing aliases.

--- vim/ui_config.vim
+++
@@ -1,7 +0,0 @@
-" enable line numbers
-  set number
-  setlocal numberwidth=3
-
-" These two enable syntax highlighting
-  set nocompatible
-  syntax on
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,42 +3,49 @@ alias ::='cd ../..'
 alias :::='cd ../../..'
 
 alias ajaxrdoc="rdoc --fmt ajax --exclude '.*generator.*' --exclude '.*test.*' --exclude '.*spec.*'"
-
 alias c='clear'
-alias cpwd="pwd | ruby -pe'\$_.chomp!' | pbcopy"
+alias cpwd="pwd | ruby -pe'\$_.chomp!' | pbcopy" # copy path to clipboard
+alias grep='grep --color --line-number'
+alias gvim='mvim -p'
+alias ll='ls -lF' # long format listing; add / to end of directories
+alias la='ls -aF' # include dot files in listing; add / to end of directories
+alias mv='mv -i' # prompt before moving a file that would overwrite an existing file
+
+
+alias icons='cd ~/Pictures/icons; open -a Preview **/*.png **/*.tiff  **/*.jpg'
+alias preview='open -a Preview'
+
+
+# Databses
+
+alias mysql='/opt/local/bin/mysql5 -u root --socket=/tmp/mysql.sock'
+alias mysqladmin='/opt/local/bin/mysqladmin5 -u root --socket=/tmp/mysql.sock'
+alias mysql_config='/opt/local/bin/mysql_config5'
+alias postgres_start='pg_ctl -D ~/.pgdata -l ~/.pgdata/psql.log start'
+alias postgres_stop='pg_ctl -D ~/.pgdata stop'
+
+# Rails
 
 alias devlog='tail -200 -f log/development.log'
-alias diff='clear; git diff'
-
-alias gfo='git fetch origin'
-alias gitrm='git ls-files --deleted | xargs git rm'
-alias gitx='gitx --all'
-alias gvim='mvim -p'
-
-alias la='ls -laFh'
-
-alias monitor_http='sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E "Host\: .*|GET \/.*"'
-alias mv='mv -i'
-alias mysql='/usr/local/mysql/bin/mysql -u root' # --socket=/tmp/mysql.sock
-alias mysqladmin='/usr/local/mysql/bin/mysqladmin -u root' # --socket=/tmp/mysql.sock
-alias mysqlstart='sudo /usr/local/mysql/bin/mysqld_safe &'
-alias mysqlstop='/usr/local/mysql/bin/mysqladmin -u root -p shutdown'
-
-alias opi='open ./public/images'
-
-alias pull='clear; git pull'
-alias push='clear; git push'
-alias pushed\?='pushed' # pushed() is defined in functions.zsh
-
+alias log='tail -f log/development.log'
 alias restart='touch tmp/restart.txt'
-alias rmate='mate app config doc db features lib public script spec test stories liquid'
 alias rc='./script/rails console'
 alias rg='./script/rails generate'
+alias rmate='mate app config doc db features lib public script spec test stories liquid'
 alias rs='./script/rails server'
-
 alias sc='./script/console'
+alias sg='./script/generate'
 alias sp='./script/spec -cfs'
 alias ss='./script/server'
-alias st='clear; git status'
-
+alias ssd='./script/server --debugger'
 alias testlog='tail -200 -f log/test.log'
+
+# Git
+
+alias gitrm='git ls-files --deleted | xargs git rm'
+alias gitx='gitx --all'
+alias gpci='git-pair commit'
+alias gp='git pair'
+alias push='git push'
+alias rebase='git fetch; git rebase origin/master'
+alias st='git status'
