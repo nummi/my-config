cf1256e7191a3e25717043ac181c021f185d4cd4
Jerry Nummi <jerry@edgecase.com>
2010-05-24 13:48:31 -0400

    Synced EdgeCase vim and zsh functions/prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,5 +1,62 @@
+# Put the string "hostname::/full/directory/path" in the title bar:
+set_term_title() { 
+  echo -ne "\e]2;$PWD\a" 
+}
+
+# Put the parentdir/currentdir in the tab
+set_term_tab() {
+  echo -ne "\e]1;$PWD:h:t/$PWD:t\a" 
+}
+
+function set_prompt() {
+  export PS1='%2/ ~ '
+
+  branch_name=$(git_branch_name)
+  author_name=$(git_author_name)
+  if [ -n "$branch_name" ]; then
+    export PS1='%1~%{$reset_color$bold_color$fg[cyan]%}%{$reset_color$fg[cyan]%} ($author_name: $branch_name)%{$reset_color%} ~ '
+    export RPS1="%{$fg[yellow]%}$(is_working_directory_dirty)$(need_push)%{$reset_color%}"
+  fi
+}
+
+function pushed() {
+  if [ $@ ]; then
+    git cherry -v origin/$@ 2>/dev/null
+  else
+    git cherry -v origin/$(git_branch_name) 2>/dev/null
+  fi
+}
+
+# Did you forget to `git push`?
+function need_push() {
+  if [[ $(pushed) == "" ]]
+  then
+    echo ""
+  else
+    echo "%{$fg[yellow]%} ↑%{\e[0m%}"
+  fi
+}
+
+function is_working_directory_dirty() {
+ if current_git_status=$(git status 2> /dev/null | grep --regex="deleted\|modified\|Untracked" 2> /dev/null); then
+   echo "⚡"
+ else
+   echo ''
+ fi
+}
+
+
+function git_branch_name() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+}
+
+function git_author_name() {
+  git config --get user.name | sed 's/\([a-zA-Z+]\)[a-zA-Z]* */\1/g' | tr '[A-Z]' '[a-z]'
+}
+
 precmd() { 
   set_term_title
   set_term_tab
   set_prompt
 }
+
