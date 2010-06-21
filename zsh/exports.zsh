28385a4e390c318859ce13012e54d81b70dc0032
Jerry Nummi <jerry@edgecase.com>
2010-06-21 16:14:05 -0400

    Zsh exports cleanup.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,4 +1,4 @@
-export PATH="/opt/local/lib/postgresql83/bin:/opt/local/bin:/bin:/sbin:/usr/bin:/usr/sbin:/sw/bin:/usr/local/bin:/usr/local/mysql/bin:/usr/local/mongodb/bin"
+export PATH=/usr/local/bin:/opt/local/bin:/bin:/sbin:/usr/bin:/usr/sbin
 export CDPATH=$CDPATH:~/Projects:~/Projects/edgecase:~/Projects/edgecase/clients
 
 export RUBYOPT='rubygems -Itest'
