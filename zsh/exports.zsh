3c8e2031dcbecb64af5e1206407c5f84ea48e260
Jerry Nummi <jerry@edgecase.com>
2011-12-12 09:49:34 -0500

    Updated exports

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,11 +1,10 @@
-export PATH=/usr/local/bin:/opt/local/bin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/sbin:/usr/local/share/npm/bin:/usr/local/git/bin:/Developer/usr/bin:/usr/local/Cellar
+export PATH=/usr/local/bin:/opt/local/bin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/sbin:/usr/local/share/npm/bin:/usr/local/git/bin:/Developer/usr/bin:/usr/local/Cellar:~/bin
 export CDPATH=$CDPATH:~/Projects:~/Projects/edgecase:~/Projects/edgecase/clients
 export NODE_PATH="/usr/local/lib/node"
 export RUBYOPT='rubygems -Itest'
 export GIT_EDITOR="vim"
 export EDITOR="mvim"
 export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Home'
-export CC=/usr/bin/gcc
 
 if [[ $TERM != 'dumb' ]]; then
   export TERM=xterm-color;
